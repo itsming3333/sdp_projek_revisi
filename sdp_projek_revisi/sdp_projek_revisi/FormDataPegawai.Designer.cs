5201dfa5637b405d402ebbc25c510676908f3e9d
Fu Ming <fuming308@yahoo.co.id>
2019-11-27 21:41:01 +0700

    master pegawai fin

--- sdp_projek_revisi/sdp_projek_revisi/FormDataPegawai.Designer.cs
+++ sdp_projek_revisi/sdp_projek_revisi/FormDataPegawai.Designer.cs
@@ -75,7 +75,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -110,7 +110,12 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Nama",
+            "NIK",
+            "ID Pegawai"});
             this.comboBox1.Location = new System.Drawing.Point(18, 108);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
@@ -154,9 +159,11 @@
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(351, 435);
             this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.dataGridView2);
@@ -185,7 +192,7 @@
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Enabled = false;
             this.groupBox1.Location = new System.Drawing.Point(376, 104);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
@@ -251,6 +258,7 @@
             this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView2.Size = new System.Drawing.Size(352, 150);
             this.dataGridView2.TabIndex = 104;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView2_CellClick);
             // 
             // groupBox3
             // 
@@ -364,7 +372,15 @@
             // 
             // comboBox2
             // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "RESEPSIONIS",
+            "DOKTER",
+            "SUSTER",
+            "APOTEKER",
+            "KASIR",
+            "WAREHOUSE"});
             this.comboBox2.Location = new System.Drawing.Point(127, 112);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(213, 21);
@@ -493,7 +509,7 @@
             // label20
             // 
             this.label20.AutoSize = true;
-            this.label20.Location = new System.Drawing.Point(29, 87);
+            this.label20.Location = new System.Drawing.Point(28, 74);
             this.label20.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(86, 13);
@@ -515,13 +531,14 @@
             // 
             this.button3.BackColor = System.Drawing.Color.Red;
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button3.Location = new System.Drawing.Point(508, 422);
+            this.button3.Location = new System.Drawing.Point(571, 422);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(58, 28);
             this.button3.TabIndex = 70;
             this.button3.Text = "Delete Member";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // label4
             // 
@@ -535,23 +552,25 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(586, 422);
+            this.button2.Location = new System.Drawing.Point(633, 422);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(56, 28);
             this.button2.TabIndex = 69;
             this.button2.Text = "Edit";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
-            // label5
+            // button1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(29, 74);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 13);
-            this.label5.TabIndex = 51;
-            this.label5.Text = "nama_pegawai";
+            this.button1.Location = new System.Drawing.Point(508, 422);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 28);
+            this.button1.TabIndex = 107;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // FormDataPegawai
             // 
@@ -597,7 +616,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label20;
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.ComboBox comboBox2;
@@ -628,5 +646,6 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
     }
 }
--- sdp_projek_revisi/sdp_projek_revisi/FormDataPegawai.cs
+++ sdp_projek_revisi/sdp_projek_revisi/FormDataPegawai.cs
@@ -36,7 +36,7 @@ namespace sdp_projek_revisi
             oda.Fill(pegawai);
             dataGridView1.DataSource = pegawai;
 
-            oda = new OracleDataAdapter("SELECT NOMOR_RUANG AS NOMOR,  NAMA_RUANG AS NAMA FROM RUANG WHERE JENIS_RUANG='KANTOR'", mainParent.oc);
+            oda = new OracleDataAdapter("SELECT NOMOR_RUANG AS NOMOR,  STATUS_RUANG AS STATUS FROM RUANG WHERE JENIS_RUANG='KANTOR'", mainParent.oc);
             DataTable ruang = new DataTable();
             oda.Fill(ruang);
             dataGridView2.DataSource = ruang;
@@ -45,6 +45,25 @@ namespace sdp_projek_revisi
         private void reset()
         {
             groupBox1.Enabled = false;
+            label14.Text = "";
+            label11.Text = "";
+            label16.Text = "";
+            label13.Text = "";
+
+            comboBox2.SelectedIndex = 0;
+            textBox2.Text = "";
+            textBox7.Text = "";
+            textBox4.Text = "";
+            textBox10.Text = "";
+            textBox9.Text = "";
+            textBox6.Text = "";
+            radioButton1.Checked = true;
+            comboBox3.SelectedIndex = 0;
+            label6.Text = "-";
+
+            label3.Text = "ID Pegawai";
+            label4.Text = "nik_pegawai";
+            label20.Text = "NPWP_pegawai";
         }
 
         public void setParent(Form1 parent)
@@ -75,6 +94,130 @@ namespace sdp_projek_revisi
         private void Label6_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int row = e.RowIndex;
+                OracleDataAdapter oda = new OracleDataAdapter("SELECT * FROM PEGAWAI WHERE NAMA_PEGAWAI='"+ dataGridView1[0, row].Value.ToString() + "'", mainParent.oc);
+                DataTable selectedPegawai = new DataTable();
+                oda.Fill(selectedPegawai);
+
+                label3.Text = selectedPegawai.Rows[0].Field<String>(0);
+                label4.Text = selectedPegawai.Rows[0].Field<String>(7);
+                label20.Text = selectedPegawai.Rows[0].Field<String>(12);
+                String id_jabatan = selectedPegawai.Rows[0].Field<String>(8);
+                OracleCommand cmd = new OracleCommand("SELECT NAMA_JABATAN FROM JABATAN WHERE ID_JABATAN='"+id_jabatan+"'", mainParent.oc);
+                String jabatan = cmd.ExecuteScalar().ToString();
+                comboBox2.Text = jabatan;
+                textBox2.Text = selectedPegawai.Rows[0].Field<String>(2);
+                textBox7.Text = selectedPegawai.Rows[0].Field<String>(4);
+                textBox4.Text = selectedPegawai.Rows[0].Field<String>(6);
+                textBox10.Text = selectedPegawai.Rows[0].Field<String>(10);
+                dateTimePicker1.Value = selectedPegawai.Rows[0].Field<DateTime>(5);
+                textBox9.Text = selectedPegawai.Rows[0].Field<String>(13);
+                textBox6.Text = selectedPegawai.Rows[0].Field<String>(14);
+                String jk = selectedPegawai.Rows[0].Field<String>(11);
+                if(jk == "L")
+                {
+                    radioButton1.Checked = true;
+                }
+                else
+                {
+                    radioButton2.Checked = true;
+                }
+                String id_kantor = selectedPegawai.Rows[0].Field<String>(9);
+                cmd = new OracleCommand("SELECT NOMOR_RUANG FROM RUANG WHERE ID_RUANG='"+id_kantor+"' AND JENIS_RUANG='KANTOR'",mainParent.oc);
+                label6.Text = cmd.ExecuteScalar().ToString();
+                comboBox3.Text = selectedPegawai.Rows[0].Field<String>(3);
+                groupBox1.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Apakah anda yakin ingin menghapus pegawai ?", "Hapus Pegawai", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                try
+                {
+                    OracleCommand cmd = new OracleCommand("DELETE FROM PEGAWAI WHERE ID_PEGAWAI='" + label3.Text + "'", mainParent.oc);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Pegawai berhasil dihapus.");
+                    showData();
+                    reset();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            String jabatan = comboBox2.Text;
+            OracleCommand cmd = new OracleCommand("SELECT ID_JABATAN FROM JABATAN WHERE NAMA_JABATAN='"+jabatan+"'", mainParent.oc);
+            String id_jabatan = cmd.ExecuteScalar().ToString();
+            String nama = textBox2.Text;
+            String telp = textBox7.Text;
+            String alamat = textBox4.Text;
+            String agama = textBox10.Text;
+            String wali = textBox9.Text;
+            String kontak_wali = textBox6.Text;
+            String dd = dateTimePicker1.Value.Date.ToString();
+            String mm = dateTimePicker1.Value.Month.ToString();
+            String yyyy = dateTimePicker1.Value.Year.ToString();
+            String jk = "";
+            if (radioButton2.Checked)
+            {
+                jk = "P";
+            }
+            else
+            {
+                jk = "L";
+            }
+            String gol = comboBox3.Text;
+            String kantor = label6.Text;
+            cmd = new OracleCommand("SELECT ID_RUANG FROM RUANG WHERE NOMOR_RUANG='"+kantor+"' AND JENIS_RUANG='KANTOR'", mainParent.oc);
+            String id_ruang = cmd.ExecuteScalar().ToString();
+
+            if (MessageBox.Show("Apakah anda yakin ingin mengupdate data pegawai ?", "Update Pegawai", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                try
+                {
+                    cmd = new OracleCommand("UPDATE PEGAWAI SET NAMA_PEGAWAI='"+nama+"', TELP_PEGAWAI='"+telp+"', ALAMAT_PEGAWAI='"+alamat+"', AGAMA_PEGAWAI='"+agama+ "', TGLLAHIR_PEGAWAI=TO_DATE(LPAD('" + dd + "',2,'0')||'/'||LPAD('" + mm + "',2,'0')||'/'||LPAD('" + yyyy + "',4,'0'),'DD/MM/YYYY'), WALI_PEGAWAI='"+wali+"', KONTAK_WALI_PEGAWAI='"+kontak_wali+"', RUANG_KANTOR='"+id_ruang+"', JK_PEGAWAI='"+jk+"', GOL_DARAH_PEGAWAI='"+gol+"' WHERE ID_PEGAWAI='"+label3.Text+"'", mainParent.oc);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Berhasil mengganti data pegawai.");
+                    reset();
+                    showData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void DataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                label6.Text = dataGridView2.Rows[e.RowIndex].Cells[0].Value.ToString();
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
